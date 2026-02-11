bfe6e6869d1693c786ee2894c3ad990487ed96fe
Bhavith832 <bhavithnhce07@gmail.com>
2026-02-11 12:17:42 +0530

    Initial React + Tailwind boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-purple-600 text-white text-4xl flex items-center justify-center">
+     Basic React + Tailwind Boilerplate
+    </div>
+  )
+}
+
+export default App
